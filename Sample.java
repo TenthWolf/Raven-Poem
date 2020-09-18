bae001f3206dc6144cc2c815512af0a5ad59c6c4
TenthWolf <64280904+TenthWolf@users.noreply.github.com>
2020-09-18 11:36:23 -0400

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,97 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+public class Sample {
+
+	// Creates method for counting all words
+	// HashMap will be used to store words
+	// Iterator can be used to loop through collections. EX: .hasnext and .next
+	// throws FileNotFound to prevent errors for popping up on Scanner
+	static void countAllWords(String fileName, Map<String, Integer> text) throws FileNotFoundException {
+		Scanner file = new Scanner(new File(fileName)); // reads from the file
+		while (file.hasNext()) { // iterator loop returns true if there is another string in the file making it
+									// // print the next word
+			String line = file.next(); // starts at first word of the poem and proceeds to the next word after loop
+										// ends
+			Integer count = text.get(line); // create count to store number of appeared words
+			if (count != null) { // count has to have a value
+				count++; // increment after program runs
+			} else
+				count = 1;
+			text.put(line, count); // counts the word and how many times each word appears
+		}
+		file.close();
+	}
+
+	// Creates Method for finding the word used most
+	static void getMostUsed(String fileName, Map<String, Integer> text) throws FileNotFoundException {
+		Scanner file = new Scanner(new File(fileName));
+		while (file.hasNext()) { // check
+			String line = file.next(); // Jumps to next word
+			Integer count = text.get(line); // counts every word
+			if (count != null) { // count has to have a value
+				count++; // increment after program runs
+			} else
+				count = 1;
+			text.put(line, count);
+		}
+		file.close();
+	}
+
+	// Creates method for getting the word that appeared most
+	// Map.entrySet method returns a collection-view of the map, whose elements are
+	// of this class.
+	static int getMaxWordOccurrence(Map<String, Integer> max) {
+
+		int top = 0; // initiate top number variable
+		for (Map.Entry<String, Integer> poem : max.entrySet()) { // for loop
+			if (poem.getValue() > top) { // poem returns the value corresponding to this entry
+				top = poem.getValue(); // top receives the value from poem
+			}
+		}
+		return top;
+	}
+
+	static void removeSpecialCharacters(String c) {
+		for (int i = 0; i < c.length(); i++) {
+
+			// Finding the character whose
+			// ASCII value fall under this
+			// range
+			if (c.charAt(i) < 'A' || c.charAt(i) > 'Z' && c.charAt(i) < 'a' || c.charAt(i) > 'z') {
+
+				// erase function to erase the character
+				c = c.substring(0, i) + c.substring(i + 1);
+				i--;
+			}
+		}
+		System.out.print(c);
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+
+		// HashMap is used to store words
+		Map<String, Integer> words = new HashMap<String, Integer>();
+		countAllWords("C:\\Users\\17876\\Documents\\Raven.txt", words); // call CountAllWords method and search for
+																		// filelocation
+		words.forEach((k, v) -> System.out.println("Key: " + k + " , Value: " + v));
+
+		Map<String, Integer> repeat = new HashMap<String, Integer>();
+		getMostUsed("C:\\Users\\17876\\Documents\\Raven.txt", repeat);
+		// calls getMostUsed method and search for
+		// filelocation
+		int total = getMaxWordOccurrence(repeat); // calls maxWordOccurrence method including the HashMap variable
+		for (Entry<String, Integer> poem : repeat.entrySet()) {
+			if (poem.getValue() == total) {
+				System.out.println("The most appeared word is " + poem);
+			}
+		}
+
+	}
+
+}
